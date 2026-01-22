5d8902a97273bc1bf0753d448199971f646bd967
jahanzaib00 <jahanzaibkhursheed2002@gmail.com>
2026-01-22 13:16:31 +0500

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { generateHomeMetadata } from "./lib/seo/metadata";
 export const metadata: Metadata = {
   ...generateHomeMetadata(),
   metadataBase: new URL(
-    process.env.NEXT_PUBLIC_SITE_URL || "https://www.visaportal.online"
+    process.env.NEXT_PUBLIC_SITE_URL || "https://www.visaportal.online",
   ),
   viewport: {
     width: "device-width",
