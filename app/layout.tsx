e8cb0741d9bb05cfec1aa789b1173b506318c3c0
jahanzaib00 <jahanzaibkhursheed2002@gmail.com>
2026-03-05 22:54:31 +0500

    feat: Add Yandex.Metrika tracking script for enhanced analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,29 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Yandex.Metrika */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=107162546','ym');
+              ym(107162546,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/107162546"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </head>
       <body className="antialiased">
         <Providers>{children}</Providers>
